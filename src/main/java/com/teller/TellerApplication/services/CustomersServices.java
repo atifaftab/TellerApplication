02cb15856150b130b979a5f2a0fe7cf1b28806dc
Mohammad Atif Aftab <iamatif96@gmail.com>
2022-06-02 20:55:44 +0800

    Customers Services added

---
+++ src/main/java/com/teller/TellerApplication/services/CustomersServices.java
@@ -0,0 +1,10 @@
+package com.teller.TellerApplication.services;
+
+import com.teller.TellerApplication.entity.Customers;
+
+import java.util.List;
+
+public interface CustomersServices {
+
+    List<Customers> findAll();
+}
